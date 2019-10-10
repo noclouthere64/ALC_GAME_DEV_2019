25322c18a271f6fc3c58ce2805457c6da2aee6be
Miguel Mendoza <miguel.m668@stu.nebo.edu>
2019-10-10 10:28:22 -0600

    Got the player to shoot projectiles

--- Create With Code/Prototype 2/Assets/Scripts/PlayerController.cs
+++ Create With Code/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
     public float xRange = 10.0f;
 
+    public GameObject projectilePrefab;
+
     // Update is called once per frame
     void Update()
     {
@@ -23,5 +25,11 @@ public class PlayerController : MonoBehaviour
 
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
