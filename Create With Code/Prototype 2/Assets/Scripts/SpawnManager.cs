3b0995cb637658a35059f3a184d0b11ae8e0c13a
Miguel Mendoza <miguel.m668@stu.nebo.edu>
2019-10-14 10:09:54 -0600

    Destroy projectile and animal prefabs

---
+++ Create With Code/Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+
+            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20),
+                animalPrefabs[animalIndex].transform.rotation);
+        }        
+    }
+}
