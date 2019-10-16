abbc816a385b3c7d4af5d4db87aae013d87ca99a
Miguel Mendoza <miguel.m668@stu.nebo.edu>
2019-10-16 10:30:13 -0600

    Added random spawns and added collider for the cow.

--- Create With Code/Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Create With Code/Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 5.5f;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
 
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20),
-                animalPrefabs[animalIndex].transform.rotation);
-        }        
+    }
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
