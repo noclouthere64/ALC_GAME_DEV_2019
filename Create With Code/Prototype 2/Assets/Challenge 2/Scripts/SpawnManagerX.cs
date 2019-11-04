8a4bc99ecf0b1106daa5e94d109424e5583381fe
Miguel Mendoza <miguel.m668@stu.nebo.edu>
2019-11-03 22:44:41 -0700

    There's a delay for spawning dogs. Play can no longer spam the space bar. If the ball touches the ground "Game Over" is shown in the console. The spawn interval is now a random value between 3 and 5.

--- Create With Code/Prototype 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Create With Code/Prototype 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -10,14 +10,16 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnLimitXRight = 7;
     private float spawnPosY = 30;
 
-    private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    private float startDelay = 3.0f;
+    private float spawnInterval = 5.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        Invoke("SpawnRandomBall", spawnInterval);
     }
+
 
     // Spawn random ball at random x position at top of play area
     void SpawnRandomBall ()
